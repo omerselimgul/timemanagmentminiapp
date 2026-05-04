294e63bd468747c85367e7a3e855f7ba5d2b7fa5
Selim <omerselimgul@gmail.com>
2026-05-04 04:02:55 +0300

    debug: log env var presence

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+console.log('MONGODB_URI:', process.env.MONGODB_URI ? 'SET' : 'UNDEFINED');
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
